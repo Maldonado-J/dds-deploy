bc402c94a62d9ec9b7d518f80211d024f78ddaa9
Johana Maldonado <maldonado.johana3@gmail.com>
2023-11-07 17:21:07 -0300

    add java test

---
+++ src/test/java/AppLibrosTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppLibrosTest {
+    @Test
+    public void testAlgoQueDeberiaEstarBien() {
+        Assertions.assertEquals(1, 1);
+    }
+}
